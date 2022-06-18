8d01ce9c6afef0274110a96449ee981fbf4a053e
Luka Svetlečić <72402718+Svetla123@users.noreply.github.com>
2022-06-18 13:41:52 +0200

    Add files via upload

---
+++ app/views/index.php
@@ -0,0 +1,18 @@
+<?php
+   require APPROOT . '/views/includes/head.php';
+?>
+<div class="section-landing">
+    <div class="navbar">
+        <?php
+        require APPROOT . '/views/includes/navigation.php';
+        ?>
+    </div>
+    <div class="wrapper-landing">
+        <h1>Smart people makes notes</h1>
+        <h2>Others not that much</h2>
+    </div>
+
+    <div class="footer">
+        <p>&copy; Copyright 2022 Luka Svetlečić</p>
+    </div>
+</div>
